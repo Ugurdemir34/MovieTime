87e9135916795bed7920d1b2246853b17b7e528e
Uğur Demir <ugurdemir551@gmail.com>
2021-05-01 22:42:26 +0300

    Repository methods added

---
+++ src/MovieTime.Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using MovieTime.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTime.Core.DataAccess
+{
+    public interface IEntityRepository<T> where T: class,IEntity,new()
+    {
+        //T Get(Expression<Func<T, bool>> filter, params Expression<Func<T,object>>[] includeProperties);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter, params Expression<Func<T, object>>[] includeProperties);
+       // IList<T> GetList(Expression<Func<T, bool>> filter = null, params Expression<Func<T, object>>[] includeProperties);
+        Task<IList<T>> GetListAsync(Expression<Func<T, bool>> filter, params Expression<Func<T, object>>[] includeProperties);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+        Task<T> AddRange(List<T> Entities);
+    }
+}
