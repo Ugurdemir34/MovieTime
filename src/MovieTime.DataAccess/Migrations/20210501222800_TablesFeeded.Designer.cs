94dc577a4604d78ed24a5e511fcdfd8d7d20e22c
Uğur Demir <ugurdemir551@gmail.com>
2021-05-02 01:29:58 +0300

    New Migration Added

--- src/MovieTime.DataAccess/Migrations/20210501222800_TablesFeeded.Designer.cs
+++ src/MovieTime.DataAccess/Migrations/20210501222800_TablesFeeded.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieTime.DataAccess.Concrete.Contexts;
 
 namespace MovieTime.DataAccess.Migrations
 {
     [DbContext(typeof(MovieTimeContext))]
-    partial class MovieTimeContextModelSnapshot : ModelSnapshot
+    [Migration("20210501222800_TablesFeeded")]
+    partial class TablesFeeded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
