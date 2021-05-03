5478e7d23329bb24895a8660e8570c3704b4865e
Uğur Demir <ugurdemir551@gmail.com>
2021-05-04 02:52:24 +0300

    CategoryListDto Added

---
+++ src/MovieTime.Entities/Dtos/CategoryListDto.cs
@@ -0,0 +1,14 @@
+﻿using MovieTime.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTime.Entities.Dtos
+{
+    public class CategoryListDto
+    {
+        public List<Category> Categories { get; set; }
+    }
+}
