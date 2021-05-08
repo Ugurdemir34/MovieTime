771131d3741ccd24be09db71d6d946b1e8966b7d
ugurdemir <ugurdemir551@gmail.com>
2021-05-08 22:18:14 +0300

    Movie Link Changed

--- src/MovieTime.Feed/Program.cs
+++ src/MovieTime.Feed/Program.cs
@@ -13,7 +13,7 @@ namespace MovieTime.Feed
 {
     class Program
     {
-        static string URL = "https://www.fullhdfilmizlesene.com/film";
+        static string URL = "https://www.fullhdfilmizlesene.com/en-cok-izlenen-filmler";
         static Uri address = new Uri(URL);
         static void Main(string[] args)
         {
@@ -65,8 +65,40 @@ namespace MovieTime.Feed
                     Thumbnail = "assets/img/theme/" + film_adi+".jpg"
                 };
                 _movieService.Add(Film);
+                Console.WriteLine("Film => " + film_adi);
+            }
+            foreach (var item in searching2)
+            {
+                var film_adi = item.Attributes["title"].Value.Split("-")[0];
+                var newUrlData = tempwc.DownloadString(item.Attributes["href"].Value);
+                tempdoc.LoadHtml(newUrlData);
+                var moviedetaysection = tempdoc.DocumentNode.SelectSingleNode("//section[@class='detay-sag']");
+                var movieSummary = moviedetaysection.SelectSingleNode("//div[1]/article/p");
+                var director = moviedetaysection.SelectSingleNode("//div[2]/dl/dd[1]/span/span");
+                var actors = moviedetaysection.SelectSingleNode("//div[2]/dl/dd[2]");
+                var date = moviedetaysection.SelectSingleNode("div[2]/dl/dd[3]/a");
+                var imdb = moviedetaysection.SelectSingleNode("/html/body/div[4]/div/header/div[2]/div[1]/span");
 
-
+                var film_ozet = movieSummary.InnerHtml.Replace("<br>", " ").Replace("<strong>", " ").Replace("</strong>", " ").Replace(specialword, " ").Replace(specialword2, " ");
+                var yonetmen = director.InnerHtml;
+                var oyuncular = actors.InnerHtml;
+                var yil = date.InnerHtml.Split(" ")[0];
+                var yapimyili = new DateTime(Convert.ToInt16(yil), 1, 1);
+                var imdbpuani = imdb.InnerHtml.Replace(".", ",");
+                var Film = new Movie
+                {
+                    Title = film_adi,
+                    Description = film_ozet,
+                    CreationDate = DateTime.Now,
+                    Director = yonetmen,
+                    Actors = oyuncular,
+                    Date = yapimyili,
+                    Imdb = Convert.ToDouble(imdbpuani),
+                    Id = Guid.NewGuid(),
+                    Thumbnail = "assets/img/theme/" + film_adi + ".jpg"
+                };
+                _movieService.Add(Film);
+                Console.WriteLine("Film => " + film_adi);
             }
             Console.WriteLine("Başarıyla Eklendi !");
             Console.ReadKey();
